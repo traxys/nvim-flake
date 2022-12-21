477b26dbb3b342fa7a8d12c2408c6108d5dcbe2c
Quentin Boyer <quentin.boyer@atos.net>
2022-12-21 21:34:06 +0100

    Add basic vim options

--- config.nix
+++ config.nix
@@ -4,5 +4,19 @@
       style = "night";
       enable = true;
     };
+
+    options = {
+      termguicolors = true;
+      number = true;
+      tabstop = 4;
+      shiftwidth = 4;
+      scrolloff = 7;
+      signcolumn = "yes";
+      cmdheight = 2;
+      completopt = "menu,menuone,noselect";
+      updatetime = 300;
+      colorcolumn = "100";
+      spell = true;
+    };
   };
 }
