1712cb9f1e8dca8fa09a7fab7544e9a272dedc9f
traxys <quentin+dev@familleboyer.net>
2023-03-18 22:04:53 +0100

    Add check back, with docker

--- flake.nix
+++ flake.nix
@@ -255,6 +255,29 @@
         nixvim' = nixvim.legacyPackages."${system}";
         nvim = nixvim'.makeNixvimWithModule {inherit module pkgs;};
       in {
+        checks.launch = pkgs.stdenv.mkDerivation {
+          name = "launch-nvim";
+
+          nativeBuildInputs = [self.packages."${system}".nvim pkgs.docker-client];
+
+          dontUnpack = true;
+          # We need to set HOME because neovim will try to create some files
+          #
+          # Because neovim does not return an exitcode when quitting we need to check if there are
+          # errors on stderr
+          buildPhase = ''
+            output=$(HOME=$(realpath .) nvim -mn --headless "+q" 2>&1 >/dev/null)
+            if [[ -n $output ]]; then
+            	echo "ERROR: $output"
+              exit 1
+            fi
+          '';
+
+          # If we don't do this nix is not happy
+          installPhase = ''
+            mkdir $out
+          '';
+        };
         formatter = pkgs.alejandra;
 
         devShell = pkgs.mkShell {
