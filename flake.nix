4107c95db6233e146976d955225a11be3d958d64
traxys <quentin+dev@familleboyer.net>
2023-11-23 20:21:13 +0100

    Use rename of nixvim repo

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     nixpkgs-staging.url = "github:nixos/nixpkgs/staging";
     nixvim = {
-      url = "github:pta2002/nixvim";
+      url = "github:nix-community/nixvim";
       #url = "/home/traxys/Documents/nixvim";
       #url = "github:traxys/nixvim?ref=dev";
       inputs.nixpkgs.follows = "nixpkgs";
